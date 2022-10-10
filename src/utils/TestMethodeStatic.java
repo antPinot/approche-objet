34bbf961ed717508028694cb629c58bff5bb30c3
Anthony Pinot <pinot.anthony@gmail.com>
2022-10-10 16:25:58 +0200

    TP 4

---
+++ src/utils/TestMethodeStatic.java
@@ -0,0 +1,20 @@
+package utils;
+
+public class TestMethodeStatic {
+
+	public static void main(String[] args) {
+		
+		String chaine = "11";
+		
+		int chaineEntier = Integer.parseInt(chaine);
+		
+		System.out.println(chaineEntier);
+		
+		int a = 15;
+		
+		int b = 12;
+		
+		System.out.println(Integer.max(a, b));
+	}
+
+}
