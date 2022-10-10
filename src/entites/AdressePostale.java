4a84ab4c35b88cf4c2339c046498faaa4c1451b4
Anthony Pinot <pinot.anthony@gmail.com>
2022-10-10 11:39:16 +0200

    first commit

---
+++ src/entites/AdressePostale.java
@@ -0,0 +1,10 @@
+package entites;
+
+public class AdressePostale {
+
+	public	int numeroRue = 0;
+	public String libelleRue = "";
+	public int codePostal = 0;
+	public String ville = "";
+
+}
---
+++ src/entites/TestAdressePostale.java
@@ -0,0 +1,15 @@
+package entites;
+
+public class TestAdressePostale {
+
+	public static void main(String[] args) {
+		
+		AdressePostale adr1 = new AdressePostale();
+		adr1.numeroRue = 5;
+		adr1.libelleRue = "des Maréchaux";
+		adr1.codePostal = 44100;
+		adr1.ville = "Nantes";
+
+	}
+
+}
