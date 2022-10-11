86ae655272dc5e67b3754df2d6fcf862c70b0ec8
Anthony Pinot <pinot.anthony@gmail.com>
2022-10-11 15:50:12 +0200

    TP 5 corrigé

--- src/fr/diginamic/banque/entites/Compte.java
+++ src/fr/diginamic/banque/entites/Compte.java
@@ -2,19 +2,40 @@ package fr.diginamic.banque.entites;
 
 public class Compte {
 	
-	private long numeroDeCompte = 0;
-	private double soldeDuCompte = 0.0;
+	private long numero;
+	private double solde;
 	
-	public Compte(long numeroDeCompte, double soldeDuCompte){
-		this.numeroDeCompte = numeroDeCompte;
-		this.soldeDuCompte = soldeDuCompte;
+	public Compte(long numero, double solde){
+		this.numero = numero;
+		this.solde = solde;
 	}
-	
+
+	public long getNumero() {
+		return numero;
+	}
+
+
+
+	public void setNumero(long numero) {
+		this.numero = numero;
+	}
+
+
+
+	public double getSolde() {
+		return solde;
+	}
+
+
+	public void setSolde(double solde) {
+		this.solde = solde;
+	}
+
 	// TP 6
-	
+
 	@Override
 	public String toString() {
-		return "Compte [numeroDeCompte=" + numeroDeCompte + ", soldeDuCompte=" + soldeDuCompte;
+		return "Compte [numeroDeCompte=" + numero + ", soldeDuCompte=" + solde;
 	}
 	
 	
