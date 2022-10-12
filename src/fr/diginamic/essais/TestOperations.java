1e73746755febcbb24ae8ec06dca1fa3a17f13b3
Anthony Pinot <pinot.anthony@gmail.com>
2022-10-12 10:39:22 +0200

    Ex Operations v2

---
+++ src/fr/diginamic/essais/TestOperations.java
@@ -0,0 +1,16 @@
+package fr.diginamic.essais;
+
+import fr.diginamic.operations.Operations;
+
+public class TestOperations {
+
+	public static void main(String[] args) {
+		
+		System.out.println(Operations.calcul(5, 4, '+'));
+		System.out.println(Operations.calcul(5, 4, '-'));
+		System.out.println(Operations.calcul(5, 4, '*'));
+		System.out.println(Operations.calcul(5, 4, '/'));
+
+	}
+
+}
